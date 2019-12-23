aa3181bd4efae9369fdb8d21428e93e14682cf2c
zhenglei <zhenglei@5.cn>
2019-12-23 14:07:50 +0800

    项目

--- app/src/main/java/com/gess/world/MainActivity.kt
+++ app/src/main/java/com/gess/world/MainActivity.kt
@@ -15,8 +15,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+        fab.setOnClickListener {
+            Snackbar.make(it, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
     }
